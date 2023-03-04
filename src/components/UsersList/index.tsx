1af495dca07e11dab1fbd1b189fc23e16372b790
Cristina <artscomi-web@gmail.com>
2023-03-04 22:52:58 +0100

    feat: calc number of items per row in user-list

--- src/components/UsersList/index.tsx
+++ src/components/UsersList/index.tsx
@@ -8,19 +8,41 @@ export const UsersList: React.FC<{
 }> = ({ activeId, getUserProfiles }) => {
   const { users } = useUsers();
 
+  const getUsersPerRow = (users: User[], limit = 5) => {
+    const numRows = Math.ceil(users.length / limit);
+    const itemsPerRow = Math.ceil(users.length / numRows);
+    const outputArr = [];
+    for (let i = 0; i < numRows; i += 1) {
+      outputArr.push(users.slice(i * itemsPerRow, (i + 1) * itemsPerRow));
+    }
+    return outputArr;
+  };
+
+  const usersPerRow = getUsersPerRow(users, 5);
+
+  const getButtonsPerRow = (users: User[]) => {
+    return (
+      <div className="user-list_row">
+        {users.map((user: User) => {
+          return (
+            <button
+              key={user.id}
+              className={`user-item ${activeId === user.id ? "active" : ""}`}
+              onClick={() => getUserProfiles(user.id)}
+            >
+              {user.first_name}
+            </button>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <div className="user-list marginBottomM">
-      {users.map((user: User, index) => {
-        return (
-          <button
-            key={index}
-            className={`user-item ${activeId === user.id ? "active" : ""}`}
-            onClick={() => getUserProfiles(user.id)}
-          >
-            <p>{user.first_name}</p>
-          </button>
-        );
-      })}
+      {usersPerRow[0] && getButtonsPerRow(usersPerRow[0])}
+      {usersPerRow[1] && getButtonsPerRow(usersPerRow[1])}
+      {usersPerRow[2] && getButtonsPerRow(usersPerRow[2])}
     </div>
   );
 };
